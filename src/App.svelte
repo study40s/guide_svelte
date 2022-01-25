6ffafe1e13d1b0a9266a874a58ac3d479b8f7b8f
mh <hyunjin@oddconcepts.kr>
2022-01-25 20:18:27 +0900

    working for setting

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { Router } from "@sveltech/routify";
+	import { routes } from "@sveltech/routify/tmp/routes";
+</script>
+
+<main>
+	<Router {routes} />
+</main>
+
+<style lang="scss">	
+</style>
---
+++ src/pages/_layout.svelte
@@ -0,0 +1,27 @@
+<script>
+    import Header from "~/components/Header.svelte";
+    import Footer from "~/components/Footer.svelte";
+</script>
+
+<Header />
+
+<div class="container">
+
+</div>
+
+<Footer />
+
+<style lang="scss">
+.container {
+    position: relative;
+    max-width: 1120px;
+    margin:0 auto;
+    padding: 40px 0 0;
+    @media #{$tablet} {
+        padding: 40px 40px 0;
+    }
+    @media #{$mobile} {
+        padding: 40px 20px 0;
+    }
+}
+</style>
